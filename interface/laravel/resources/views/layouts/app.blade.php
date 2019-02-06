6c6909a98f9298ed636b7ea312b04ed880944db8
Freelancer <32210677+EuFreela@users.noreply.github.com>
2019-02-06 01:22:03 -0200

    Update app.blade.php

--- interface/laravel/resources/views/layouts/app.blade.php
+++ interface/laravel/resources/views/layouts/app.blade.php
@@ -52,6 +52,11 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+                            
+                                <li class="nav-item active">
+                                    <a class=" btn btn-success" href="{{route('dracula.hosts.start')}}">Start <span class="sr-only">(current)</span></a>
+                                </li>
+                                
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
