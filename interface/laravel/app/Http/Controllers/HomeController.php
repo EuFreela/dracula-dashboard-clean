772ebe72f337516a87d69c93ddc66fcd39149713
Freelancer <32210677+EuFreela@users.noreply.github.com>
2019-02-06 01:23:29 -0200

    Update HomeController.php

--- interface/laravel/app/Http/Controllers/HomeController.php
+++ interface/laravel/app/Http/Controllers/HomeController.php
@@ -29,4 +29,15 @@ class HomeController extends Controller
             'hosts' => DB::table('hosts')->count()
             ]);
     }
+    
+    public function getHostsStart()
+    {
+        if( DB::table('reload')->update(['status'=>1]) )
+            return redirect()->route('home')
+                ->with('success',DB::table('systemsg')->where('code','=',3)->first()->msg);
+
+        return redirect()->back()
+            ->with('error',DB::table('systemsg')->where('code','=',4)->first()->msg);    
+    }
+    
 }
