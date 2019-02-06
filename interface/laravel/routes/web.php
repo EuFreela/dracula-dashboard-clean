adb024f30e6e64691908684de43286d1312512d0
Freelancer <32210677+EuFreela@users.noreply.github.com>
2019-02-06 01:22:46 -0200

    Update web.php

--- interface/laravel/routes/web.php
+++ interface/laravel/routes/web.php
@@ -18,3 +18,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'],  
+    function(){
+               
+        Route::get('/hosts/start','HomeController@getHostsStart')->name('dracula.hosts.start');
+
+    });
